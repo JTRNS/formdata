295d138d76754f7382b592adea1f3f0d8a6675a1
Jeroen Trines <jeroen@trines.info>
2023-01-07 15:08:51 +0100

    Move parsing of entire collection into mod.ts

--- parse_collection.ts
+++ parse_collection.ts
@@ -1,31 +1,14 @@
-type FormDataValue =
+export type ParsedFormDataValue =
   | string
   | number
   | boolean
   | File
   | Date
-  | null
-  | FormDataValue[];
+  | null;
 
-/**
- * Parses a collection of form data entries and returns an object with the same keys, but with the values parsed according to their type.
- * @param obj An object with keys representing form data entry names and values that are either a single `FormDataEntryValue` or an array of `FormDataEntryValue`s.
- * @returns An object with keys for each unique key in the input object, and values that are either a single `FormDataValue` or an array of `FormDataValue`s.
- */
-export function parseFormDataCollection(
-  obj: Record<string, FormDataEntryValue | FormDataEntryValue[]>
-) {
-  const entries: [string, FormDataValue][] = Object.entries(obj).map(
-    ([key, value]) => {
-      return Array.isArray(value)
-        ? [key, value.map(parseFormDataValue)]
-        : [key, parseFormDataValue(value)];
-    }
-  );
-  return Object.fromEntries(entries);
-}
-
-function parseFormDataValue(value: FormDataEntryValue) {
+export function parseFormDataValue(
+  value: FormDataEntryValue
+): ParsedFormDataValue {
   if (isFile(value)) {
     return value;
   } else if (isBoolean(value)) {
